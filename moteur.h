169b242fff6e45d850a53eb408451c48468f50c9
CIEL25_admin <jouquandfabien@gmail.com>
2026-09-03 10:17:00 +0200

    modification du fichier moteur.h avec commentaire doxygen

---
+++ moteur.h
@@ -0,0 +1,37 @@
+#ifndef MOTEUR_H
+#define MOTEUR_H
+
+/**
+ * @class Moteur
+ * @brief Classe mère représentant un moteur.
+ */
+class Moteur {
+private:
+    int puissance;
+
+public:
+    /**
+     * @brief Constructeur
+     * @param puissance Puissance du moteur en watts
+     */
+    Moteur(int puissance = 0);
+
+    /**
+     * @brief Destructeur virtuel (évite les fuites mémoire avec l'héritage)
+     */
+    virtual ~Moteur();
+
+    /**
+     * @brief Getter pour la puissance
+     * @return Puissance du moteur
+     */
+    int getPuissance() const;
+
+    /**
+     * @brief Setter pour la puissance
+     * @param puissance Nouvelle puissance
+     */
+    void setPuissance(int puissance);
+};
+
+#endif
